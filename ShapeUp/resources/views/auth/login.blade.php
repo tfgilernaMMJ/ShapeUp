25af3bac8576325c15d1334f4ecea02d50930b6c
Manuel Moya <manuel.moyavadillo2@gmail.com>
2023-05-02 21:39:21 +0200

    feat: commfirmation email message

--- ShapeUp/resources/views/auth/login.blade.php
+++ ShapeUp/resources/views/auth/login.blade.php
@@ -5,67 +5,69 @@ Inicio sesión
 @endsection
 
 @section('login-section')
-	<div class="limiter">
-		<div class="container-login100">
-			<div class="wrap-login100">
-				<form class="login100-form validate-form"  action="{{route('login')}}" method="POST">
-					@csrf
-					<span class="login100-form-title p-b-43">
-						Inicio sesión
-					</span>
-					
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-						<input class="input100" type="text" name="email">
-						<span class="focus-input100"></span>
-						<span class="label-input100">Correo electrónico</span>
-					</div>
-					
-					<div class="wrap-input100 validate-input" data-validate="Password is required">
-						<input class="input100" type="password" name="pass">
-						<span class="focus-input100"></span>
-						<span class="label-input100">Contraseña</span>
-					</div>
+<div class="limiter">
+	<div class="container-login100">
+		<div class="wrap-login100">
+			<form class="login100-form validate-form" action="{{route('login')}}" method="POST">
+				@csrf
+				<span class="login100-form-title p-b-43">
+					Inicio sesión
+				</span>
 
-					<div class="flex-sb-m w-full p-t-3 p-b-32">
-						<div class="contact100-form-checkbox">
-							<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-							<label class="label-checkbox100" for="ckb1">
-								Recordarme
-							</label>
-						</div>
+				<div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+					<input class="input100" type="text" name="email">
+					<span class="focus-input100"></span>
+					@error('email')
+					<p>{{$message}}</p>
+					@enderror
+					<span class="label-input100">Correo electrónico</span>
+				</div>
 
-						<div>
-							<a href="#" class="txt1">
-								¿Has olvidado la contraseña?
-							</a>
-						</div>
-					</div>
+				<div class="wrap-input100 validate-input" data-validate="Password is required">
+					<input class="input100" type="password" name="password">
+					<span class="focus-input100"></span>
+					@error('password')
+					<p>{{$message}}</p>
+					@enderror
+					<span class="label-input100">Contraseña</span>
+				</div>
 
-                    <div class="flex-sb-m w-full p-t-3 p-b-32">
-						<div>
-							<p class="txt1">
-								¿No tienes cuenta? Regístrate <a href="{{route('signup')}}" class="txt1">aquí</a>.
-							</p>
-						</div>
+				<div class="flex-sb-m w-full p-t-3 p-b-32">
+					<div>
+						<a href="#" class="txt1">
+							¿Has olvidado la contraseña?
+						</a>
 					</div>
-			
+				</div>
 
-					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
-							Iniciar sesión
-						</button>
+				<div class="flex-sb-m w-full p-t-3 p-b-32">
+					<div>
+						<p class="txt1">
+							¿No tienes cuenta? Regístrate <a href="{{route('signup')}}" class="txt1">aquí</a>.
+						</p>
 					</div>
-					
-					<!-- <div class="text-center p-t-46 p-b-20">
+				</div>
+
+
+				<div class="container-login100-form-btn">
+					<button class="login100-form-btn">
+						Iniciar sesión
+					</button>
+				</div>
+
+				<!-- <div class="text-center p-t-46 p-b-20">
 						<span class="txt2">
 							¿Ya tienes una cuenta? Inicia sesión
 						</span>
 					</div> -->
-				</form>
+			</form>
 
-				<div class="login100-more" style="background-image: url({{ asset('auth2/images/login.jpeg') }});">
-                </div>
+			<div class="login100-more" style="background-image: url({{ asset('auth/images/login.jpeg') }});">
+				<div class="glitch-wrapper">
+					<div class="glitch" data-glitch="ShapeUp">ShapeUp</div>
+				</div>
 			</div>
 		</div>
 	</div>
+</div>
 @endsection
