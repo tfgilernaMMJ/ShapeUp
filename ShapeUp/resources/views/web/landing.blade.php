3c25a561a12eaf530029d46f266f0482faa13b00
Mario Rufo Ariza <mariojra6@gmail.com>
2023-05-05 00:34:13 +0200

    Landing

--- ShapeUp/resources/views/web/landing.blade.php
+++ ShapeUp/resources/views/web/landing.blade.php
@@ -2,37 +2,31 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>ShapeUp - LandingPage</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+    <title>ShapeUp | Conócenos</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
 
-  <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="{{ asset('web/assets/img/logo/favicon.png') }}" rel="icon">
+    <link href="{{ asset('web/assets/img/logo/favicon.png') }}" rel="apple-touch-icon">
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    {{-- <link href="assets/vendor/aos/aos.css" rel="stylesheet"> --}}
+    <link href="{{ asset('landing/assets/vendor/aos/aos.css') }}" rel="icon">
+    <link href="{{ asset('landing/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="icon">
+    <link href="{{ asset('landing/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="icon">
+    <link href="{{ asset('landing/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="icon">
+    <link href="{{ asset('landing/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="icon">
+    {{-- <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> --}}
+    {{-- <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet"> --}}
+    {{-- <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet"> --}}
+    {{-- <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet"> --}}
 
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: SoftLand
-  * Updated: Mar 10 2023 with Bootstrap v5.2.3
-  * Template URL: https://bootstrapmade.com/softland-bootstrap-app-landing-page-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
+    <link href="{{ asset('landing/assets/css/style.css') }}" rel="icon">
+    {{-- <link href="assets/css/style.css" rel="stylesheet"> --}}
 </head>
 
 <body>
@@ -309,14 +303,18 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="{{ asset('landing/assets/vendor/aos/aos.js') }}"></script>
+  {{-- <script src="assets/vendor/aos/aos.js"></script> --}}
+  <script src="{{ asset('landing/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('landing/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+  <script src="{{ asset('landing/assets/vendor/php-email-form/validate.js') }}"></script>
+  {{-- <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script> --}}
+  {{-- <script src="assets/vendor/swiper/swiper-bundle.min.js"></script> --}}
+  {{-- <script src="assets/vendor/php-email-form/validate.js"></script> --}}
 
   <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+  <script src="{{ asset('landing/assets/js/main.js') }}"></script>
+  {{-- <script src="assets/js/main.js"></script> --}}
 
 </body>
-
 </html>
