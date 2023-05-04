8935c816db26995f80b2d054b2587c4e9e2c3679
Manuel Moya <manuel.moyavadillo2@gmail.com>
2023-05-05 00:32:31 +0200

    refractor: change location of public files of dashboard

--- ShapeUp/resources/views/dashboard/templates/general.blade.php
+++ ShapeUp/resources/views/dashboard/templates/general.blade.php
@@ -26,21 +26,21 @@
     <!--     Fonts and icons     -->
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
     <!-- Nucleo Icons -->
-    <link href="{{asset('web/assets/css/nucleo-icons.css')}}" rel="stylesheet" />
-    <link href="{{asset('web/assets/css/nucleo-svg.css')}}" rel="stylesheet" />
+    <link href="{{asset('dashboard/css/nucleo-icons.css')}}" rel="stylesheet" />
+    <link href="{{asset('dashboard/css/nucleo-svg.css')}}" rel="stylesheet" />
     <!-- Font Awesome Icons -->
     <script defer src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
     <!-- Material Icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
     <!-- CSS Files -->
-    <link id="pagestyle" href="{{asset('web/assets/css/material-dashboard.css?v=3.1.0')}}" rel="stylesheet" />
+    <link id="pagestyle" href="{{asset('dashboard/css/material-dashboard.css?v=3.1.0')}}" rel="stylesheet" />
     <!--   Core JS Files   -->
-    <script defer src="{{ asset('web/assets/js/core/bootstrap.min.js') }}"></script>
-    <script defer src="{{ asset('web/assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
-    <script defer src="{{ asset('web/assets/js/core/popper.min.js') }}"></script>
-    <script defer src="{{ asset('web/assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
-    <script defer src="{{ asset('web/assets/js/plugins/chartjs.min.js') }}"></script>
-    <script defer src="{{ asset('web/assets/js/material-dashboard.min.js?v=3.1.0') }}"></script>
+    <script defer src="{{ asset('dashboard/js/core/bootstrap.min.js') }}"></script>
+    <script defer src="{{ asset('dashboard/js/plugins/perfect-scrollbar.min.js') }}"></script>
+    <script defer src="{{ asset('dashboard/js/core/popper.min.js') }}"></script>
+    <script defer src="{{ asset('dashboard/js/plugins/smooth-scrollbar.min.js') }}"></script>
+    <script defer src="{{ asset('dashboard/js/plugins/chartjs.min.js') }}"></script>
+    <script defer src="{{ asset('dashboard/js/material-dashboard.min.js?v=3.1.0') }}"></script>
     <!-- Nepcha Analytics (nepcha.com) -->
     <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
     <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
