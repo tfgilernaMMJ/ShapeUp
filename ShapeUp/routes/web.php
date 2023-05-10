73e97080a5763f0d885f05d4c492abc1148282be
Mario Rufo Ariza <mariojra6@gmail.com>
2023-05-10 20:20:20 +0200

    Añadiendo middleware para más seguridad

--- ShapeUp/routes/web.php
+++ ShapeUp/routes/web.php
@@ -27,7 +27,8 @@ Route::get('/', [App\Http\Controllers\WebController::class, 'index'])->name('acc
 
 Route::get('/home', function () {
     return view('web.landing');
-})->name('account.landing');
+})->name('account.landing')->middleware('auth');
+
 // AUTH
 
 Route::get('/signin', function () {
@@ -48,84 +49,84 @@ Route::get('/landing', function () {
 
 Route::get('/imc', function () {
     return view('web.imc');
-})->name('account.imc');
+})->name('account.imc')->middleware('auth');
 
 // ABOUT
 
 Route::get('/conocenos', function () {
     return view('web.about');
-})->name('account.about');
+})->name('account.about')->middleware('auth');
 
 // COURSES
 
-Route::get('/entrenamientos', [App\Http\Controllers\WebController::class, 'indexTrainings'])->name('account.trainings');
+Route::get('/entrenamientos', [App\Http\Controllers\WebController::class, 'indexTrainings'])->name('account.trainings')->middleware('auth');
 
-Route::get('/entrenamientos/ejercicios/{training_id}', [App\Http\Controllers\WebController::class, 'indexTrainingsExercises'])->name('account.trainings.exercises');
+Route::get('/entrenamientos/ejercicios/{training_id}', [App\Http\Controllers\WebController::class, 'indexTrainingsExercises'])->name('account.trainings.exercises')->middleware('auth');
 
-Route::get('/entrenamientos/accion/{action}/{training_id}', [App\Http\Controllers\WebController::class, 'followTrainings'])->name('account.trainings.follow');
+Route::get('/entrenamientos/accion/{action}/{training_id}', [App\Http\Controllers\WebController::class, 'followTrainings'])->name('account.trainings.follow')->middleware('auth');
 
 // COACHES 
 
-Route::get('/entrenadores', [App\Http\Controllers\WebController::class, 'indexCoaches'])->name('account.coaches');
+Route::get('/entrenadores', [App\Http\Controllers\WebController::class, 'indexCoaches'])->name('account.coaches')->middleware('auth');
 
-Route::get('/entrenadores/accion/{action}/{coach_id}', [App\Http\Controllers\WebController::class, 'followCoaches'])->name('account.coaches.follow');
+Route::get('/entrenadores/accion/{action}/{coach_id}', [App\Http\Controllers\WebController::class, 'followCoaches'])->name('account.coaches.follow')->middleware('auth');
 
-Route::get('/entrenadores/mensajes/{coach_id}', [App\Http\Controllers\WebController::class, 'messageCoaches'])->name('account.coaches.message');
+Route::get('/entrenadores/mensajes/{coach_id}', [App\Http\Controllers\WebController::class, 'messageCoaches'])->name('account.coaches.message')->middleware('auth');
 
-Route::post('/entrenadores/enviarmensajes/{coach_id}', [App\Http\Controllers\WebController::class, 'sendMessageCoaches'])->name('account.coaches.message.send');
+Route::post('/entrenadores/enviarmensajes/{coach_id}', [App\Http\Controllers\WebController::class, 'sendMessageCoaches'])->name('account.coaches.message.send')->middleware('auth');
 
 // EVENTS
 
 Route::get('/eventos', function () {
     return view('web.events');
-})->name('account.events');
+})->name('account.events')->middleware('auth');
 
 // SUBSCRIPTIONS
 Route::get('/suscripciones', function () {
     return view('web.subscriptions');
-})->name('account.subscriptions');
+})->name('account.subscriptions')->middleware('auth');
 
 Route::get('/suscripciones/metodo-de-pago', function () {
     return view('web.checkout');
-})->name('account.checkout');
+})->name('account.checkout')->middleware('auth');
 
 Route::get('/suscripciones/metodo-de-pago/paypal', function () {
     return view('web.paypal');
-})->name('account.paypal');
+})->name('account.paypal')->middleware('auth');
 
 Route::get('/suscripciones/metodo-de-pago/tarjeta-de-credito', function () {
     return view('web.creditcard');
-})->name('account.creditcard');
+})->name('account.creditcard')->middleware('auth');
 
-Route::post('/suscripciones/metodo-de-pago/pago', [App\Http\Controllers\WebController::class, 'paymentSubscription'])->name('account.payment');
+Route::post('/suscripciones/metodo-de-pago/pago', [App\Http\Controllers\WebController::class, 'paymentSubscription'])->name('account.payment')->middleware('auth');
 
 Route::get('/suscripciones/metodo-de-pago/confirm', function () {
     return view('web.confirmdisable');
-})->name('account.confirmdisable');
+})->name('account.confirmdisable')->middleware('auth');
 
-Route::get('/suscripciones/metodo-de-pago/{action}', [App\Http\Controllers\WebController::class, 'paymentSubscription'])->name('account.payment.disable');
+Route::get('/suscripciones/metodo-de-pago/{action}', [App\Http\Controllers\WebController::class, 'paymentSubscription'])->name('account.payment.disable')->middleware('auth');
 
 // CONTACT
 
 Route::get('/contacto', function () {
     return view('web.contact');
-})->name('account.contact');
+})->name('account.contact')->middleware('auth');
 
-Route::post('/contacto/enviarcorreo/', [App\Http\Controllers\WebController::class, 'contactShapeUp'])->name('account.contact.email.send');
+Route::post('/contacto/enviarcorreo/', [App\Http\Controllers\WebController::class, 'contactShapeUp'])->name('account.contact.email.send')->middleware('auth');
 
 // PROFILE
 
 Route::get('/perfil', function () {
     return view('web.profile');
-})->name('account.profile');
+})->name('account.profile')->middleware('auth');
 
-Route::post('/perfil/editar/', [App\Http\Controllers\WebController::class, 'editProfile'])->name('account.profile.edit');
+Route::post('/perfil/editar/', [App\Http\Controllers\WebController::class, 'editProfile'])->name('account.profile.edit')->middleware('auth');
 
 Route::get('/perfil/cambiar-contraseña', function () {
     return view('web.editpassword');
-})->name('account.profile.password');
+})->name('account.profile.password')->middleware('auth');
 
-Route::post('/perfil/editar/cambiar-contraseña', [App\Http\Controllers\WebController::class, 'editPassword'])->name('account.profile.edit.password');
+Route::post('/perfil/editar/cambiar-contraseña', [App\Http\Controllers\WebController::class, 'editPassword'])->name('account.profile.edit.password')->middleware('auth');
 
 // ------------------------------------------------------------
 
@@ -133,74 +134,78 @@ Route::post('/perfil/editar/cambiar-contraseña', [App\Http\Controllers\WebContr
 
 // ------------------------------------------------------------
 
-Route::get('/admin-entrenador', [App\Http\Controllers\CoachController::class, 'dashboardPrincipal'])->name('admin-coach');
+Route::get('/admin-entrenador', [App\Http\Controllers\CoachController::class, 'dashboardPrincipal'])->name('admin-coach')->middleware('auth', 'coach');
 
 Route::get('/admin-entrenador/trainings', function () {
     return view('coach.trainings');
-})->name('admin-coach.trainings');
-Route::get('/search',[App\Http\Controllers\CoachController::class, 'searchTrainings'])->name('search-trainings');
+})->name('admin-coach.trainings')->middleware('auth', 'coach');
+
+Route::get('/search',[App\Http\Controllers\CoachController::class, 'searchTrainings'])->name('search-trainings')->middleware('auth', 'coach');
 
 Route::get('/admin-entrenador/formularios', function () {
     return view('coach.forms');
-})->name('admin-coach.forms');
+})->name('admin-coach.forms')->middleware('auth', 'coach');
 
 Route::get('/admin-entrenador/tarjetas', function () {
     return view('coach.cards');
-})->name('admin-coach.cards');
+})->name('admin-coach.cards')->middleware('auth', 'coach');
 
 Route::get('/admin-entrenador/graficos', function () {
     return view('coach.charts');
-})->name('admin-coach.charts');
+})->name('admin-coach.charts')->middleware('auth', 'coach');
 
 Route::get('/admin-entrenador/botones', function () {
     return view('coach.buttons');
-})->name('admin-coach.buttons');
+})->name('admin-coach.buttons')->middleware('auth', 'coach');
 
 Route::get('/admin-entrenador/modales', function () {
     return view('coach.modals');
-})->name('admin-coach.modals');
+})->name('admin-coach.modals')->middleware('auth', 'coach');
 
 Route::get('/admin-entrenador/tablas', function () {
     return view('coach.tables');
-})->name('admin-coach.tables');
-
-
-// COACH DASHBOARD
+})->name('admin-coach.tables')->middleware('auth', 'coach');
 
 // ------------------------------------------------------------
 
-Route::get('/admin', [App\Http\Controllers\AdminController::class, 'dashboardPrincipal'])->name('admin');
-Route::get('/search',[App\Http\Controllers\AdminController::class, 'dashboardPrincipal'])->name('admin-search');
+// ADMIN DASHBOARD
 
-Route::get('/admin-coaches',[App\Http\Controllers\AdminController::class, 'dashboardCoaches'])->name('admin.coaches');
+// ------------------------------------------------------------
+
+Route::get('/admin', [App\Http\Controllers\AdminController::class, 'dashboardPrincipal'])->name('admin')->middleware('auth', 'admin');
+Route::get('/search',[App\Http\Controllers\AdminController::class, 'dashboardPrincipal'])->name('admin-search')->middleware('auth', 'admin');
+
+Route::get('/admin-coaches',[App\Http\Controllers\AdminController::class, 'dashboardCoaches'])->name('admin.coaches')->middleware('auth', 'admin');
 
 Route::get('/admin-trainings', function () {
     return view('admin.trainings');
-})->name('admin.trainings');
+})->name('admin.trainings')->middleware('auth', 'admin');
 
 Route::get('/admin-formularios', function () {
     return view('admin.forms');
-})->name('admin.forms');
+})->name('admin.forms')->middleware('auth', 'admin');
 
 Route::get('/admin-tarjetas', function () {
     return view('admin.cards');
-})->name('admin.cards');
+})->name('admin.cards')->middleware('auth', 'admin');
 
 Route::get('/admin-graficos', function () {
     return view('admin.charts');
-})->name('admin.charts');
+})->name('admin.charts')->middleware('auth', 'admin');
 
 Route::get('/admin-botones', function () {
     return view('admin.buttons');
-})->name('admin.buttons');
+})->name('admin.buttons')->middleware('auth', 'admin');
 
 Route::get('/admin-modales', function () {
     return view('admin.modals');
-})->name('admin.modals');
+})->name('admin.modals')->middleware('auth', 'admin');
 
 Route::get('/admin-tablas', function () {
     return view('admin.tables');
-})->name('admin.tables');
+})->name('admin.tables')->middleware('auth', 'admin');
+
+// ------------------------------------------------------------
 
 // Auth::routes(['verify' => true]);
 
