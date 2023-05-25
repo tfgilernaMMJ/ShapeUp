3fd6a1be2a1b06bde797a4a1b2ad7f41f2b4d3c4
Mario Rufo Ariza <mariojra6@gmail.com>
2023-05-25 22:08:40 +0200

    Index admin actualizado

--- ShapeUp/resources/views/admin/index.blade.php
+++ ShapeUp/resources/views/admin/index.blade.php
@@ -24,9 +24,9 @@ text-gray-800 dark:text-gray-100
         <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20">
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
         </svg>
-        <span>See this project on GitHub</span>
+        <span>Mira nuestro proyecto en GitHub</span>
       </div>
-      <span>View more &RightArrow;</span>
+      <span>Ver más &RightArrow;</span>
     </a>
     <!-- Cards -->
     <div class="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
@@ -56,7 +56,7 @@ text-gray-800 dark:text-gray-100
 
     <!-- Charts -->
   <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-    Info
+    Información ShapeUp
   </h2>
   <div class="grid gap-6 mb-8 md:grid-cols-2">
     <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
