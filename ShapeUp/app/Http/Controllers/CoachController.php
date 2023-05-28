8d785ca850cb51aeeb2db00434d197eacb9a070b
Manuel Moya <manuel.moyavadillo2@gmail.com>
2023-05-28 13:56:10 +0200

    feat: edit coach from coach dashboard added

--- ShapeUp/app/Http/Controllers/CoachController.php
+++ ShapeUp/app/Http/Controllers/CoachController.php
@@ -7,11 +7,13 @@ use App\Models\Exercise;
 use App\Models\Ingredient;
 use App\Models\Training;
 use App\Models\User;
+use Brian2694\Toastr\Facades\Toastr;
 use Illuminate\Http\Request;
 use App\Models\UserFollowCoach;
 use App\Models\UserFollowDiet;
 use App\Models\UserFollowTraining;
 use Illuminate\Support\Facades\Auth;
+use PDOException;
 
 class CoachController extends Controller
 {
@@ -21,13 +23,13 @@ class CoachController extends Controller
         $trainings = Training::all()->count();
         $diets = Diet::all()->count();
         $exercises = Exercise::all()->count();
-        $trainingsOfCoachCount = Training::where('user_coach_id',Auth()->user()->id)->count();
-        $exercisesOfCoachCount = Exercise::where('user_coach_id',Auth()->user()->id)->count();
-        $dietsOfCoachCount = Diet::where('user_coach_id',Auth()->user()->id)->count();
-        $followers = UserFollowCoach::where('user_coach_id',Auth()->user()->id)->count();
+        $trainingsOfCoachCount = Training::where('user_coach_id', Auth()->user()->id)->count();
+        $exercisesOfCoachCount = Exercise::where('user_coach_id', Auth()->user()->id)->count();
+        $dietsOfCoachCount = Diet::where('user_coach_id', Auth()->user()->id)->count();
+        $followers = UserFollowCoach::where('user_coach_id', Auth()->user()->id)->count();
         $ingredientsCount = Ingredient::all()->count();
         $cardsResults = array($trainingsOfCoachCount, $exercisesOfCoachCount, $dietsOfCoachCount, $followers, $trainings, $exercises, $ingredientsCount, $diets);
-        
+
         foreach ($cardsResults as &$result) {
             if (is_null($result)) {
                 $result = null;
@@ -47,24 +49,24 @@ class CoachController extends Controller
             'p-3 mr-4 text-green-500 bg-green-100 rounded-full dark:text-green-100 dark:bg-green-500'
         ];
 
-        $diets = Diet::where('user_coach_id',Auth()->user()->id)->get();
-        $trainings = Training::where('user_coach_id',Auth()->user()->id)->get();
+        $diets = Diet::where('user_coach_id', Auth()->user()->id)->get();
+        $trainings = Training::where('user_coach_id', Auth()->user()->id)->get();
 
-        $followersByMonth = UserFollowCoach::where('user_coach_id',Auth()->user()->id)->get()->groupBy(function ($user) {
+        $followersByMonth = UserFollowCoach::where('user_coach_id', Auth()->user()->id)->get()->groupBy(function ($user) {
             return $user->created_at->format('m');
         });
         $followersDietByMonth = UserFollowDiet::whereIn('diet_id', $diets->pluck('id'))
-        ->get()
-        ->groupBy(function ($diet) {
-            return $diet->created_at->format('m');
-        });
-        
+            ->get()
+            ->groupBy(function ($diet) {
+                return $diet->created_at->format('m');
+            });
+
         $followersTrainingByMonth = UserFollowTraining::whereIn('training_id', $trainings->pluck('id'))
-        ->get()
-        ->groupBy(function ($training) {
-            return $training->created_at->format('m');
-        });
-    
+            ->get()
+            ->groupBy(function ($training) {
+                return $training->created_at->format('m');
+            });
+
         return view(
             'coach.index',
             [
@@ -93,4 +95,72 @@ class CoachController extends Controller
         ]);
     }
 
+
+    public function editCoachView(Request $request)
+    {
+        $coach = User::where('id', Auth()->user()->id)->first();
+
+        return view('coach.users.editCoach', [
+            'coach' => $coach,
+        ]);
+    }
+    public function editCoach(Request $request)
+    {
+        try {
+            // Obtener los datos del formulario
+            $name = $request->input('name');
+            $username = $request->input('username');
+            $password = $request->input('password');
+            $email = $request->input('email');
+            $age = $request->input('age');
+            $height = $request->input('height');
+            $weight = $request->input('weight');
+            $biography = $request->input('biography');
+            $experience = $request->input('experience');
+
+            // Obtener el modelo del entrenador
+            $coach = User::where('id', Auth::user()->id)->first();
+
+            // Actualizar los atributos del modelo con los nuevos datos
+            $coach->name = $name;
+            $coach->username = $username;
+            $coach->password = bcrypt($password); // Recuerda encriptar la contraseña si es necesario
+            $coach->email = $email;
+            $coach->age = $age;
+            $coach->height = $height;
+            $coach->weight = $weight;
+            // Procesar y guardar la foto si se ha proporcionado
+            if ($request->file('photo')) {
+                $file = $request->file('photo');
+                $destinationPath = 'dashboard/assets/img/test';
+                $filename = $coach->id . '.' . $file->getClientOriginalExtension();
+                $existingFiles = glob(public_path($destinationPath) . '/' . $coach->id . '.*');
+
+                [$width, $height] = getimagesize($file);
+            
+                if ($width != $height) {
+                    Toastr::error('La foto debe ser de dimensión 1:1', 'Error', ["positionClass" => "toast-top-center", "timeOut" => "4000", "progressBar" => true]);
+                    return back();
+                }
+
+                foreach ($existingFiles as $existingFile) {
+                    if (is_file($existingFile)) {
+                        unlink($existingFile);
+                    }
+                }
+                $file->move(public_path($destinationPath), $filename);
+                $coach->photo = $filename;
+            }
+            $coach->biography = $biography;
+            $coach->experience = $experience;
+
+            // Guardar los cambios en la base de datos
+            $coach->save();
+            Toastr::success('Entrenador editado', 'Exito', ["positionClass" => "toast-top-center", "timeOut" => "4000", "progressBar" => true]);
+            return back();
+        } catch (PDOException  $e) {
+            Toastr::error($e->getMessage(), 'Error', ["positionClass" => "toast-top-center", "timeOut" => "5000", "progressBar" => true]);
+            return redirect()->back();
+        }
+    }
 }
--- ShapeUp/resources/views/coach/users/dashCoaches.blade.php
+++ ShapeUp/resources/views/coach/users/dashCoaches.blade.php
@@ -46,14 +46,14 @@ text-gray-800 dark:text-gray-100
                             </td>
                             @if(!empty($coach->email))
                             <td class="px-4 py-3 text-xs">
-                                <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
+                                <span class="px-2 py-1 font-semibold leading-tight text-blue-500 bg-blue-100 rounded-full dark:text-blue-100 dark:bg-blue-500">
                                     {{$coach->email}}
                                 </span>
                             </td>
                             @endif
                             @if(!empty($coach->age))
                             <td class="px-4 py-3 text-xs">
-                                <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
+                                <span class="px-2 py-1 font-semibold leading-tight text-teal-500 bg-teal-100 rounded-full dark:text-teal-100 dark:bg-teal-500">
                                     {{$coach->age}}
                                 </span>
                             </td>
@@ -61,23 +61,18 @@ text-gray-800 dark:text-gray-100
                             
                             @if(!empty($coach->country))
                                 <td class="px-4 py-3 text-sm">
-                                    <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
+                                    <span class="px-2 py-1 font-semibold leading-tight text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500">
                                         {{$coach->country}}
                                     </span>
                                 </td>
                             @endif
                             <td class="px-4 py-3">
+                                @if(Auth::user()->id == $coach->id)
                                 <div class="flex items-center space-x-4 text-sm">
                                     
                                         <button data-bs-toggle="modal" data-bs-target="#modalEditId-{{$coach->id}}" class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Edit" data-toggle="modal" data-target="#editModal-{{$coach->id}}">
                                             <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                                                 <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
-                                            </svg>
-                                        </button>
-                                        <button data-bs-toggle="modal" data-bs-target="#modalDeleteId-{{$coach->id}}" class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Delete" data-toggle="modal" data-target="#deleteModal-{{$coach->id}}">
-
-                                            <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                                                <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
                                             </svg>
                                         </button>
 
@@ -95,37 +90,13 @@ text-gray-800 dark:text-gray-100
                                                     </div>
                                                 </div>
                                                 <div class="modal-footer">
-                                                        <a href="" class="btn btn-secondary">Editar</a>
+                                                        <a href="{{route('coach.editCoachView')}}" class="btn btn-secondary">Editar</a>
                                                     <button type="button" class="btn btn-light" data-bs-dismiss="modal">Cerrar</button>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
 
-                                    <!-- Modal -->
-                                    <div class="modal fade" id="modalDeleteId-{{$coach->id}}" tabindex="-1" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
-                                        <div class="modal-dialog" role="document">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h5 class="modal-title" id="modalTitleId">Entrenador - Eliminar</h5>
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <div class="container-fluid">
-                                                        ¿Desea eliminar?
-                                                    </div>
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <form action="" method="POST">
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <button type="submit" class="btn btn-secondary" data-bs-dismiss="modal">Eliminar</button>
-                                                    </form>
-                                                    <button type="button" class="btn btn-light">Cerrar</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
 
                                     <script>
                                         var modaEditlId = document.getElementById('modalEdit-{{$coach->id}}');
@@ -138,20 +109,11 @@ text-gray-800 dark:text-gray-100
 
                                             // Use above variables to manipulate the DOM
                                         });
-                                        var modalDelete = document.getElementById('modalDelete-{{$coach->id}}');
-
-                                        modalDelete.addEventListener('show.bs.modal', function(event) {
-                                            // Button that triggered the modal
-                                            let button = event.relatedTarget;
-                                            // Extract info from data-bs-* attributes
-                                            let recipient = button.getAttribute('data-bs-whatever');
-
-                                            // Use above variables to manipulate the DOM
-                                        });
                                     </script>
 
 
                                 </div>
+                            @endif
                             </td>
                         </tr>
                         @endforeach
