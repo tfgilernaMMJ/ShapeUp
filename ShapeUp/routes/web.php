666d0921c3ad53cadd4741d7d5c13d17edfd127d
Mario Rufo Ariza <mariojra6@gmail.com>
2023-05-29 01:54:57 +0200

    intento ajax

--- ShapeUp/routes/web.php
+++ ShapeUp/routes/web.php
@@ -160,7 +160,7 @@ Route::get('/perfil/resetear', [App\Http\Controllers\WebController::class, 'rese
 
 Route::get('/mensajería', [App\Http\Controllers\WebController::class, 'indexMessaging'])->name('account.messaging')->middleware('auth', 'usersuper');
 
-Route::post('/mensajería/check', [App\Http\Controllers\WebController::class, 'actualizarCheck'])->name('account.messaging.check')->middleware('auth', 'usersuper');
+Route::post('/mensajeria/check', [App\Http\Controllers\WebController::class, 'actualizarCheck'])->name('account.messaging.check')->middleware('auth', 'usersuper');
 
 // ------------------------------------------------------------
 
